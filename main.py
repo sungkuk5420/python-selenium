ca3b42aef47f725abf1bbef2554fb89172933277
sungkuk5420 <sungkuk5420@gmail.com>
2023-02-05 00:41:39 +0900

    나이키 크롤링 완성

--- main.py
+++ main.py
@@ -6,8 +6,14 @@ import time
 import urllib.request
 import googletrans
 from googletrans import Translator
+import csv
+
 base_url = "https://www.nike.com/jp/w/mens-lifestyle-shoes-13jrmznik1zy7ok"
-driver = webdriver.Chrome()# 같은경로라면 드라이버 경로를 안써도 된다.
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+options.add_argument('--disable-blink-features=AutomationControlled')
+driver = webdriver.Chrome(chrome_options=options)# 같은경로라면 드라이버 경로를 안써도 된다.
 
 alreadyLinks = list()
 productCode = ""
@@ -16,18 +22,17 @@ def printLog(logMessage):
     print("==================================")
     print(logMessage)
 
-async def getInfo(links):
-    for currentLinkTag in links:
-        currentLink = currentLinkTag.get_attribute("href")
-        print(currentLink)
-        await driver.get(currentLink)
-        await time.sleep(2)
-        await driver.back()
-
+def waitProductListPageLoading():
+    detailPage = driver.page_source.find('experience-wrapper')
+    if detailPage != -1:
+        return False
+    else:
+        return True
 
 def isFindingProduct(alreadyLinks):
-    printLog(base_url+"페이지로 이동")
-    driver.get(base_url) 
+    while(waitProductListPageLoading()): #product
+        printLog("상품 리스트 페이지 로딩중...") #product
+    printLog("리스트 페이지 로딩완료") #product
     FoundItem = None
     # print(listItems)
     links = driver.find_elements(By.CLASS_NAME,"product-card__img-link-overlay")
@@ -37,7 +42,7 @@ def isFindingProduct(alreadyLinks):
         currentIndex = alreadyLinks.index(currentLinkText) if currentLinkText in alreadyLinks else -1
         if(currentIndex == -1):
             print("상품 발견: " + currentLinkText + "//")
-            FoundItem = currentLinkTag
+            FoundItem = currentLinkText
             break
         else:
             print("이미 등록된상품입니다!!")
@@ -56,17 +61,15 @@ def waitProductDetailPageLoading():
     else:
         return True
 
-def moveToCurrentProductDetailPage(currentProduct):
-    currentProduct.click()
+def moveToCurrentProductDetailPage(currentLinkText):
+    driver.get(currentLinkText)
     time.sleep(3)
     while(waitProductDetailPageLoading()): #product
         printLog("상품 상세 페이지 로딩중...") #product
     printLog("상세 페이지 로딩완료") #product
 
 
-def getCurrentProductInfo():
-    time.sleep(2)
-
+def getCurrentProductInfo(link):
     root  = driver.find_element(By.CLASS_NAME,"css-mso6zd")
     title  = root.find_element(By.ID,"pdp_product_title")
     titleText  = title.get_attribute("textContent")
@@ -76,21 +79,33 @@ def getCurrentProductInfo():
     titleText =  translator.translate(titleText, dest='ko').text
     subTitleText =  translator.translate(subTitleText, dest='ko').text
     price  = root.find_element(By.XPATH,"//div[@data-test='product-price']")
-    priceText  = price.get_attribute("textContent")
+    priceText  = price.get_attribute("textContent").replace("￥","")
     sizes = list()
+    counts= list()
     images = list()
-    sizeTagsParent  = root.find_element(By.CLASS_NAME,"css-12whm6j").find_elements(By.TAG_NAME,"div")
+    if(driver.page_source.find('css-12whm6j') != -1):
+        sizeTagsParent  = root.find_element(By.CLASS_NAME,"css-12whm6j").find_elements(By.TAG_NAME,"div")
+    if(driver.page_source.find('css-1j3x2vp') != -1):
+        sizeTagsParent  = root.find_element(By.CLASS_NAME,"css-1j3x2vp").find_elements(By.TAG_NAME,"div")
     for currentTag in sizeTagsParent:
-        sizeInput = currentTag.find_element(By.TAG_NAME,"input")
+        sizeInput = currentTag.find_element(By.TAG_NAME,"input")          
+        sizeText = currentTag.find_element(By.TAG_NAME,"label").get_attribute("textContent")
+        sizeText=sizeText.replace("[","").replace("'","").replace("JP","")
+        if(sizeText.find(".")!=-1):
+            sizeText=sizeText.replace(".","")
+        else:
+            sizeText=sizeText.replace(" ","").replace("(US5)","").replace("(US6)","")+"0"
+        sizeText=sizeText.replace(" ","").replace("(US5)","").replace("(US6)","")
+        sizeText = sizeText.replace(" ","")
+        print(sizeText)
         if(sizeInput.get_attribute("disabled") == None):
-            sizeText = currentTag.find_element(By.TAG_NAME,"label").get_attribute("textContent")
-            sizeText=sizeText.replace("[","").replace("'","").replace("JP","")
-            if(sizeText.find(".")!=-1):
-                sizeText=sizeText.replace(".","")
-            else:
-                sizeText=sizeText.replace(" (US 5)","").replace(" (US 6)","")+"0"
             sizes.append(sizeText)
+            counts.append("5")
+        else:
+            sizes.append(sizeText)
+            counts.append("0")
     productCode = driver.find_element(By.CLASS_NAME,"description-preview__style-color").get_attribute("textContent").replace("スタイル： ", "")            
+    origin = driver.find_element(By.CLASS_NAME,"description-preview__origin").get_attribute("textContent")
     # imageTags  = driver.find_element(By.CLASS_NAME,"css-1rayx7p").find_elements(By.TAG_NAME,"img")
     imageTags  = driver.find_element(By.CLASS_NAME,"css-1rayx7p").find_elements(By.XPATH,"//*[@data-sub-type='image']")
     index = 1
@@ -98,38 +113,83 @@ def getCurrentProductInfo():
         imageText = currentTag.find_elements(By.TAG_NAME,"picture")[1].find_element(By.TAG_NAME,"img").get_attribute("src")
         if(index ==5):
             urllib.request.urlretrieve(imageText, productCode  +".png")
-        images.append(imageText)
+        # if(index ==6):
+        #     urllib.request.urlretrieve(imageText, productCode  +"(1).png")
+        if(imageText!=None):
+            imageText = f"<img src='{imageText}'/>"
+            images.append(imageText)
         index = index +1
 
     productInfo = {
+        "link": link,
         "productCode": productCode,
         "title": titleText,
         "subTitle": subTitleText,
         "price": priceText,
         "sizes": sizes,
+        "counts": counts,
         "images": images,
+        "origin": origin,
     }
     return productInfo
 
 def fileSave(productInfo):
     print(productInfo)
-    file = open("result.csv", "w", encoding="utf-8-sig")
+    file = open("result.csv", "a", encoding="utf-8-sig")
     # string = f"{productInfo['productCode']},{productInfo['title']},{productInfo['subTitle']},{productInfo['price']},{productInfo['sizes']},{productInfo['images']}"
-    string = f"{productInfo['productCode']},{productInfo['title']},{productInfo['subTitle']},{productInfo['price']}"
+    string = f'{productInfo["link"]},{productInfo["productCode"]},{productInfo["productCode"]}.png,{productInfo["title"]},{productInfo["subTitle"]},"{productInfo["price"]}"'
+    sizeString=""
+    countString=""
+    imageString=""
     for currentSize in productInfo['sizes']:
-        string = string+","+currentSize
+        sizeString  =sizeString + currentSize + ","
+    sizeString = '"'+sizeString+'"'
+    for currentCount in productInfo['counts']:
+        countString  =countString + currentCount + ","
+    countString = '"'+countString+'"'
+    print(countString)
+    string = string+","+sizeString
+    string = string+","+countString
     for currentImage in productInfo['images']:
-        string = f"{string},{currentImage}"
+        imageString  =imageString + currentImage + ","
+    imageString = '"'+imageString+'"'
+    string = string+","+'"'+productInfo['images'][4]+'"'
+    string = string+","+imageString
+    string = string+","+productInfo['origin']
+    # for currentImage in productInfo['images']:
+    #     string = f'{string},"{currentImage}"'
+    string = f'{string}\n'
     file.write(string)
+    
+allProduct = list()
+def mainFunc():
+    
+    printLog(base_url+"페이지로 이동")
+    driver.get(base_url) 
 
-while(isFindingProduct(alreadyLinks) == False):
-    printLog("재등록 상품 찾는중...")
+    file = open("./result.csv", 'r', encoding="utf-8-sig")
+    csvreader = csv.reader(file)
+    print(csvreader)
+    for row in csvreader:
+        print(row)
+        if(row!=[]):
+            alreadyLinks.append(row[0])
+    # while(isFindingProduct(alreadyLinks) == False):
+    #     printLog("재등록 상품 찾는중...")
 
-moveToCurrentProductDetailPage(isFindingProduct(alreadyLinks))
+    link = isFindingProduct(alreadyLinks)
+    moveToCurrentProductDetailPage(link)
 
 
-productInfo = getCurrentProductInfo()
-fileSave(productInfo)
+    productInfo = getCurrentProductInfo(link)
+    allProduct.append(productInfo )
+    # print(productInfo)
+    print(alreadyLinks)
+    fileSave(productInfo)
+    mainFunc()
+
+mainFunc()
+# fileSave(productInfo)
 
 # app = Flask("JobScrapper")
 
